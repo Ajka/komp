d3c6c6b3223bb6f5eead4a4331e3b66145e6c6d4
Ajka <pisarcikova.alena@gmail.com>
2014-10-20 22:28:20 +0200

    3 sample programs

---
+++ library.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string>
+     
+void read(int n;) {
+    scanf("%d",&n); 
+    return n;
+}
+
+void print(int n){
+    printf("%d\n",n);
+} 
+
+void print(bool b){
+ if(b){
+    printf("YES");
+ }else{
+    printf("NO");
+ }
+    
+} 
+
